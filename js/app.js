0c14893c22baca6fcaeaf2a0de6202c87e8613e9
nelyflores <nely.afloresr@gmail.com>
2017-05-04 17:22:00 -0500

    funcionando el modal

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 /*
  * Archivo principal de JS
  */
- var intputElements=document.getElementsByClassName('formulario');
- function validacion(){
- for (var i=0;i <intputElements.length; i++){
-   if (intputElements.value==null || valor.length == 0 || /^\s+$/.test(valor) ){
-     console.log("faltan cosas")
-   }
+ var img = document.getElementsByClassName('imagen');
+	 console.log(img);
+	 var longitud=img.length;
+	 console.log(longitud)
+	 for(var i=0;i<longitud; i++){
+	 	img[i].addEventListener( 'click', openModal);
+	 }
+	 function openModal(){
+	 	console.log("clic")
+	 	document.getElementById('myModal').style.display="block";
 
- }
-}
+	 	document.getElementById("imagensota").src= this.src;
+	 }
+	 var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+			document.getElementById("myModal").style.display = "none";
+		}
