a025bc4fdeddf1de68439a13e29fc0c98f7f7d4f
nelyflores <nely.afloresr@gmail.com>
2017-05-04 17:08:39 -0500

    arreglando estrellas y botones con hover

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 /*
  * Archivo principal de JS
  */
+ var intputElements=document.getElementsByClassName('formulario');
+ function validacion(){
+ for (var i=0;i <intputElements.length; i++){
+   if (intputElements.value==null || valor.length == 0 || /^\s+$/.test(valor) ){
+     console.log("faltan cosas")
+   }
+
+ }
+}
